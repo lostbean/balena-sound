1d4b3bf53fb763ba8d43d2f9a373dec423953865
lostbean <talktoedgar@gmail.com>
2021-05-10 10:57:25 -0300

    add plugin

---
+++ plugins/amp-power/src/index.ts
@@ -0,0 +1,34 @@
+import BalenaAudio from './BalenaAudio'
+
+const PULSE_SERVER = process.env.PULSE_SERVER
+
+export default async function main () {
+
+  // Connect to audio block server
+  let client = new BalenaAudio(PULSE_SERVER)
+  console.log(await client.listen())
+
+  // Listen for play/stop events
+  client.on('play', (data: any) => {
+    console.log('Started playing!')
+    console.log(data)
+  })
+  client.on('stop', (data: any) => {
+    console.log('Stopped playing!')
+    console.log(data)
+  })
+
+  // Set volume to 100%
+  await client.setVolume(100)
+
+  // // Play with a decreasing volume pattern
+  // let vol = 100
+  // setInterval(async () => {
+  //   await client.setVolume(vol)
+  //   vol = vol === 0 ? 100 : vol - 10
+  //   console.log(`Volume is ${await client.getVolume()}%`)
+  // }, 500)
+}
+
+
+main()
